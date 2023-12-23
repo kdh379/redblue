82827265cef84639fb7c93c5db91cebd065062d7
kdh379 <ehgus37625@naver.com>
2023-12-23 18:28:45 +0900

    feat: 문제별 nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
+import Link from "next/link";
+
+const NAV_LIST = [
+    {
+        href: "/scrolling",
+        text: "2.1 Infinite Scrolling",
+    },
+    {
+        href: "/sign-up",
+        text: "2.2 Sign Up",
+    },
+    {
+        href: "/jwt-authentication",
+        text: "2.3 JWT Authentication",
+    },
+];
+
 export default function Home() {
-    return <></>;
+    return (
+        <nav className="p-4">
+            <ul>
+                {NAV_LIST.map((nav) => (
+                    <li key={nav.href}>
+                        <Link
+                            href={nav.href}
+                            className="hover:text-blue-800 active:text-blue-400"
+                        >
+                            <strong>{nav.text}</strong>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
 }
