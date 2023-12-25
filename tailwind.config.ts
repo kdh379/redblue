e64808c9e9f0ae087e9dd867f1c3df1df1e507f5
kdh379 <ehgus37625@naver.com>
2023-12-25 19:04:48 +0900

    feat: daisyui 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,6 +29,14 @@ const config: Config = {
             },
         },
     },
-    plugins: [],
+    plugins: [require("daisyui")],
+    daisyui: {
+        base: true, // applies background color and foreground color for root element by default
+        styled: true, // include daisyUI colors and design decisions for all components
+        utils: true, // adds responsive and modifier utility classes
+        rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+        prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+        logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    },
 };
 export default config;
